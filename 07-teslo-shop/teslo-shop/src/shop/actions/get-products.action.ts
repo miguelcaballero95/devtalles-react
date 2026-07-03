6a647b4f17e09e908c9804ce60ed4a4eba9da2a9
Miguel Caballero <mc.caballero95@gmail.com>
2026-07-02 22:32:12 -0400

    add product, user, and response interfaces; update getProductsAction type

--- 07-teslo-shop/teslo-shop/src/shop/actions/get-products.action.ts
+++ 07-teslo-shop/teslo-shop/src/shop/actions/get-products.action.ts
@@ -1,6 +1,7 @@
 import {tesloApi} from "@/api/teslo.api.ts";
+import type {ProductsResponse} from "@/interfaces/products.response.ts";
 
 export const getProductsAction = async () => {
-  const {data} = await tesloApi.get('/products');
+  const {data} = await tesloApi.get<ProductsResponse>('/products');
   return data;
 }
