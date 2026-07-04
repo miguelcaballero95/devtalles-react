51630e09889d248a2a3ee447dd67967be73024d5
Miguel Caballero <mc.caballero95@gmail.com>
2026-07-04 12:21:30 -0400

    update authentication flow with Zustand; add `checkAuthStatus` method and integrate with `CheckAuthProvider`

--- 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
+++ 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
@@ -3,16 +3,19 @@ import {appRouter} from "./app.router"
 import {QueryClient, QueryClientProvider, useQuery} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {Toaster} from "sonner";
-import {checkAuthAction} from "@/auth/actions/check-auth.action.ts";
 import type {PropsWithChildren} from "react";
 import {CustomFullScreenLoading} from "@/components/custom/CustomFullScreenLoading.tsx";
+import {useAuthStore} from "@/auth/store/auth.store.ts";
 
 const queryClient = new QueryClient();
 
 const CheckAuthProvider = ({children}: PropsWithChildren) => {
+
+  const {checkAuthStatus} = useAuthStore();
+
   const {isLoading} = useQuery({
     queryKey: ['auth'],
-    queryFn: checkAuthAction,
+    queryFn: checkAuthStatus,
     retry: false,
     refetchInterval: 1000 * 60 * 1.5,
     refetchOnWindowFocus: true
--- 07-teslo-shop/teslo-shop/src/auth/store/auth.store.ts
+++ 07-teslo-shop/teslo-shop/src/auth/store/auth.store.ts
@@ -1,6 +1,7 @@
 import {create} from "zustand";
 import type {User} from "@/interfaces/user.interface.ts";
 import {loginAction} from "@/auth/actions/login.action.ts";
+import {checkAuthAction} from "@/auth/actions/check-auth.action.ts";
 
 type AuthStatus = 'authenticated' | 'unauthenticated' | 'loading';
 
@@ -11,6 +12,7 @@ type AuthState = {
 
   login: (email: string, password: string) => Promise<boolean>;
   logout: () => void;
+  checkAuthStatus: () => Promise<boolean>;
 }
 
 export const useAuthStore = create<AuthState>((setState) => ({
@@ -19,22 +21,31 @@ export const useAuthStore = create<AuthState>((setState) => ({
   authStatus: 'loading',
 
   login: async (email: string, password: string) => {
-
-    console.log({email, password});
-
     try {
       const data = await loginAction(email, password);
       localStorage.setItem('token', data.token);
-      setState({user: data.user, token: data.token});
+      setState({user: data.user, token: data.token, authStatus: 'authenticated'});
       return true;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
-      setState({user: null, token: null});
+      setState({user: null, token: null, authStatus: 'unauthenticated'});
       localStorage.removeItem('token');
       return false;
     }
   },
   logout: () => {
-    setState({user: null, token: null});
+    setState({user: null, token: null, authStatus: 'unauthenticated'});
     localStorage.removeItem('token');
   },
+  checkAuthStatus: async () => {
+    try {
+      const {user, token} = await checkAuthAction();
+      setState({user, token, authStatus: 'authenticated'});
+      return true;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      setState({user: null, token: null, authStatus: 'unauthenticated'});
+      return false;
+    }
+  }
 }));
